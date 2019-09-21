83c36d6e86b82ddab01924a3d59057eee9cf064c
Veto Ramirez <vetorobert3@gmail.com>
2019-09-21 08:44:04 -0700

    exported one const from data.js file and imported to index.js.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+//imported from data.js
+import { numbers } from '../data.js'
+
 ReactDOM.render(<App />, document.getElementById("root"));
