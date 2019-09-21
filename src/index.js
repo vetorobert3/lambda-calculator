dc8e48b8e5908140fc2c46d1bec1d107c571381f
Veto Ramirez <vetorobert3@gmail.com>
2019-09-21 08:52:14 -0700

    correcred the file path from data.js to index.js. inports are connected.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 
 //imported from data.js
-import { numbers } from '../data.js'
+import { numbers } from './data.js'
+import { operators } from './data.js'
+import { specials } from './data.js'
 
 ReactDOM.render(<App />, document.getElementById("root"));
