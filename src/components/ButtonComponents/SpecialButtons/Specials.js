e70a5b38cdd688cc16b04f09e919f047e243186f
Veto Ramirez <vetorobert3@gmail.com>
2019-09-24 20:48:17 -0700

    Got the special buttons to show on the calculator.

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
-import { specials } from './data.js'
+import { specials } from '../../../data.js';
+import SpecialButton from './SpecialButton.js';
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
@@ -13,6 +14,11 @@ const Specials = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {specials.map((spcl, key) => {
+         return <SpecialButton spcl={spcl} key={key}/>
+       })}
     </div>
   );
 };
+
+export default Specials;
